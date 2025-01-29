fe43e1f709be3a68a2a49e78221d9e3e3c3e5bad
unknown <bhargavraj1395@gmail.com>
2025-01-29 12:48:54 +0530

    ProductCard

---
+++ E-Commerce-Application-main/Frontend/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import {Route, Routes} from 'react-router-dom'
+import {Login} from  './Component/Login'
+import {Signup} from './Component/Signup'
+import {Home} from './page/Home'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/Login" element={<Login/>}></Route>
+        <Route path="/sign-up" element={<Signup/>}></Route>
+      </Routes>
+    </>
+  )
+}
+
+export default App
